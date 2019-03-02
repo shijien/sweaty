002ca0f99b40d982565aee051099bf7d5bbf925c
shijien <ning.shijie.develop@gmail.com>
2019-03-01 17:41:16 -0800

    Prepare for initial Heroku push

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
 
 
     def reset_session_token!
-        # debugger
+        
         self.session_token = SecureRandom::urlsafe_base64
         self.save!
         self.session_token
