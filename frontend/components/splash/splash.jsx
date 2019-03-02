ec61bba8cc3b583c2f9095b0302d622d5c925e99
shijien <ning.shijie.develop@gmail.com>
2019-03-01 18:18:37 -0800

    Heroku

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 class Splash extends React.Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
         this.handleClick = this.handleClick.bind(this);
     }
 
